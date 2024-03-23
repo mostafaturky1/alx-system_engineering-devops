5c94e7e63372ecb3e5f8516252b15b82cecfd78c
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2024-03-23 02:15:59 +0000

    “commit”

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/usr/local/bin', '/usr/bin'],
-  environment => ['PIP_DISABLE_PIP_VERSION_CHECK=1'],
-  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require     => Package['python3-pip'],
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
