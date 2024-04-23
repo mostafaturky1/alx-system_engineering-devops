4f964cb517787e983e79911e0005c9417708e5c9
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-04-23 04:00:08 +0200

    “commit”

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+
+    urlGet = requests.get(url + "/todos")
+    urlGet2 = requests.get(url)
+
+    todos = urlGet.json()
+    name = urlGet2.json()
+    file = f"{sys.argv[1] + ".csv"}"
+    with open(file, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        for todo in todos:
+            p = (sys.argv[1], name['name'], todo['completed'], todo['title'])
+            csv_writer.writerow(p)
