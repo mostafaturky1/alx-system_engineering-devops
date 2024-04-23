df4806452736e1df51de60e3172bb0ed293f0dfa
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-04-23 02:52:40 +0200

    “commit”

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" module """
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+if __name__ == "__main__":
+    url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
 
-urlGet = requests.get(url + "/todos")
-urlGet2 = requests.get(url)
+    urlGet = requests.get(url + "/todos")
+    urlGet2 = requests.get(url)
 
-todos = urlGet.json()
-name = urlGet2.json()
-complete = 0
-for todo in todos:
-    if todo['completed'] is True:
-        complete += 1
-print(f"Employee {name['name']} is done with tasks({complete}/{len(todos)}):")
-for todo in todos:
-    if todo['completed'] is True:
-        print(f"\t {todo['title']}")
+    todos = urlGet.json()
+    name = urlGet2.json()
+    complete = 0
+    for todo in todos:
+        if todo['completed'] is True:
+            complete += 1
+    print(f"Employee {name['name']} is done with tasks({complete}/{len(todos)}):")
+    for todo in todos:
+        if todo['completed'] is True:
+            print(f"\t {todo['title']}")
