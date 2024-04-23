391bed6df602772b2b7db806797488e8f3596a4d
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-04-23 02:41:44 +0200

    “commit”

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module """
+import requests
+import sys
+
+
+urlGet = requests.get
+(f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos")
+urlGet2 = requests.get
+(f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}")
+
+todos = urlGet.json()
+name = urlGet2.json()
+complete = 0
+for todo in todos:
+    if todo['completed'] is True:
+        complete += 1
+print(f"Employee {name['name']} is done with tasks({complete}/{len(todos)}):")
+for todo in todos:
+    if todo['completed'] is True:
+        print(f"     {todo['title']}")
