20883253b0cd86e0971a5b0c7b73659b80bf5938
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-05-08 03:32:06 +0300

    “commit”

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Returns the number of subscribers (not active users,
+    total subscribers) for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {'User-Agent': 'MyAPI/0.0.4'}
+    params = {"limit": 10}
+    response = requests.get(url, params=params,
+                            headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()['data']['children']
+        for child in data:
+            print(child['data']['title'])
+        return
+    print(None)
