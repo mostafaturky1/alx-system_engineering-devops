baa203a9c9e4b23182281ad08710cb94900d942e
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-05-08 03:33:49 +0300

    “commit”

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+1-top_ten
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    headers = {'User-Agent': 'MyAPI/0.0.4'}
+    params = {"limit": 10}
+    response = requests.get(url, params=params,
+                            headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()['data']['children']
+        for child in data:
+            print(child['data']['title'])
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    print(None)
