87df4de5c9dadf14a76aeb2875ae41d874b5e049
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-05-08 03:56:52 +0300

    “commit”

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,8 +8,10 @@ after = None
 
 def recurse(subreddit, hot_list=[]):
     """
-    Returns a list containing the titles of all hot articles for a given subreddit.
-    If no results are found for the given subreddit, the function should return None.
+    Returns a list containing the titles of all hot articles
+    for a given subreddit.
+    If no results are found for the given subreddit,
+    the function should return None.
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     global after
